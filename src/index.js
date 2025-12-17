43cca4cc93cd3d2385174a66b1958b836b7bbac8
Innocent866 <igoldima@gmail.com>
2025-12-17 17:27:20 +0100

    first update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://discipline-frontend-seven.vercel.app"],
+  origin: ["https://discipline-frontend-seven.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
